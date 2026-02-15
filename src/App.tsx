d8db565dfdf72b9a5b39cd42bee89689ea6ab731
Ten-Dark <hromaplay844@gmail.com>
2026-02-15 12:24:39 +0300

    chore: add Prettier config and format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import {Button, Card, Input} from "./shared/ui";
+import { Button, Card, Input } from './shared/ui'
 
 export const App = () => {
-    return (
-        <div className={'flex items-center gap-x-2'}>
-            <Card variant={'default'} padding='sm'> <Button
-                variant={'primary'}
-                size='md'
-                fullWidth={false}
-            > Добавленные </Button> </Card>
+  return (
+    <div className={'flex items-center gap-x-2'}>
+      <Card variant={'default'} padding="sm">
+        {' '}
+        <Button variant={'primary'} size="md" fullWidth={false}>
+          {' '}
+          Добавленные{' '}
+        </Button>{' '}
+      </Card>
 
-            <Card variant={'bordered'} padding='lg'> <Button
-                variant={'outline'}
-                size='lg'
-                fullWidth={false}
-            > Button </Button>
-
-                <Input
-                    label={'email'}
-                    type={'email'}
-                    placeholder={'email@example.com'}
-                    helperText={'We\'ll never share your email'}
-                ></Input> </Card>
-
-        </div>
-
-    )
-};
+      <Card variant={'bordered'} padding="lg">
+        {' '}
+        <Button variant={'outline'} size="lg" fullWidth={false}>
+          {' '}
+          Button{' '}
+        </Button>
+        <Input
+          label={'email'}
+          type={'email'}
+          placeholder={'email@example.com'}
+          helperText={"We'll never share your email"}
+        ></Input>{' '}
+      </Card>
+    </div>
+  )
+}
